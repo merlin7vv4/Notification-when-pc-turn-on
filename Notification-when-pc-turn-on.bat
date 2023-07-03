56a07bcfb3f2502761a51c114602b16dba95e61a
Merlin <68115291+merlin7vv4@users.noreply.github.com>
2023-07-03 19:47:44 +0700

    Update Notification-when-pc-turn-on.bat

--- Notification-when-pc-turn-on.bat
+++ Notification-when-pc-turn-on.bat
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001
-curl -H "Accept: application/json" -H "Content-Type:application/json; Charset=utf-8" -X POST --data "{\"content\": \"Bật máy\"}" https://discord.com/api/webhooks/1125350519527968879/KxGmDKXhQ2uPatdVPbczzdVvWOfg1ad6CcywJ-ldgruHEUisB_2TQeWEaSqH2QXPteDR
+curl -H "Accept: application/json" -H "Content-Type:application/json; Charset=utf-8" -X POST --data "{\"content\": \"Bật máy\"}" WEBHOOK_URL
